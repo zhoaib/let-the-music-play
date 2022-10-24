94b791d08bfe7d58e3c94de28563d01f2d236840
zhoaib <zhoaibhassan@gmail.com>
2022-10-24 22:46:46 +0600

    folders

---
+++ src/Pages/Shared/SideNav/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideNav = () => {
+    return (
+        <div>
+            <h2>sidenav</h2>
+        </div>
+    );
+};
+
+export default SideNav; 
---
+++ src/layout/Main.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Header from '../Pages/Shared/Header/Header';
+import SideNav from '../Pages/Shared/SideNav/SideNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+
+                    <Col lg="9">
+                        <Outlet></Outlet>
+                    </Col>
+
+                    <Col lg="3">
+                        <SideNav></SideNav>
+                    </Col>
+
+                </Row>
+            </Container>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
