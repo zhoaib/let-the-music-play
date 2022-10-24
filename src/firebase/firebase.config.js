2d79dd73a1aaf305ae9f155e8ec9de497cc308c1
zhoaib <zhoaibhassan@gmail.com>
2022-10-24 20:59:11 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC5dQK7uvtmlSdZ_2xR-oM2qqbJUX03bUg",
+    authDomain: "let-the-music-play-74042.firebaseapp.com",
+    projectId: "let-the-music-play-74042",
+    storageBucket: "let-the-music-play-74042.appspot.com",
+    messagingSenderId: "166862987627",
+    appId: "1:166862987627:web:ad9d41293457e1fd3cac2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
